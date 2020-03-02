a05939555c52b68fba96916602e2209d6faa6c81
Zoe C. | Venn <38826285+vennyoot@users.noreply.github.com>
2020-03-02 17:19:30 -0500

    player type enum updated in Movement.cs

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerType { Sitter, B1, B2 }
+
+    public PlayerType playerType;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveByPlayerType();
+    }
+
+    void MoveByPlayerType()
+    {
+        switch (playerType)
+        {
+            case PlayerType.B1:
+
+                InputHandle.GetBabyInteractKey();
+                InputHandle.GetSwitchKey();
+                break;
+
+            case PlayerType.B2:
+
+                InputHandle.GetBabyInteractKey();
+                InputHandle.GetSwitchKey();
+                break;
+
+            case PlayerType.Sitter:
+
+                InputHandle.GetSitterInteractKey();
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/InputHandle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputHandle
+{
+    public static bool GetBabyInteractKey()
+    {
+        return Input.GetKey(KeyCode.A);
+    }
+
+    public static bool GetSitterInteractKey()
+    {
+        return Input.GetKey(KeyCode.LeftControl);
+    }
+
+    public static bool GetSwitchKey()
+    {
+        return Input.GetKey(KeyCode.S);
+    }
+}
