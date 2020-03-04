11678ea9f1c0d5456f5e488442ba3d9b59698da2
Bearicade <spellshot@yahoo.ca>
2020-03-04 02:57:39 -0500

    house meter init commit

---
+++ Assets/Scripts/Components/HouseMeter.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class HouseMeter : MonoBehaviour
+{
+    public HouseMeter other;
+    
+    Image meter;
+    float prev = 0f;
+    public float percent = 0f;
+    float lerpPercent = 0f;
+    bool update = false;
+    float offset = 0.7f;
+    public float startFill = 0;
+
+    RectTransform rect;
+    public UnityEvent onEmpty;
+    public UnityEvent onFull;
+
+    [SerializeField]
+    float increment = 5;
+
+    private void Awake()
+    {
+        meter = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
+
+        Add(startFill);
+    }
+
+    void Update()
+    {
+        if (lerpPercent < 1 && update == true)
+        {
+            lerpPercent += 1 / 10f;
+        }
+        else
+        {
+            update = false;
+        }
+
+        meter.fillAmount = Mathf.Lerp(prev, percent, lerpPercent);
+        //Debug.Log(lerpPercent);
+
+    }
+
+    public void Add(float magnitude)
+    {
+        prev = percent;
+        percent += increment / 100f * (magnitude * 20);//12);
+        lerpPercent = 0f;
+        update = true;
+
+        if (percent > 1)
+        {
+            percent = 1;
+        }
+    }
+
+    public void Sub(float magnitude)
+    {
+        prev = percent;
+        percent -= increment / 100f * (magnitude * 20);
+        lerpPercent = 0f;
+        update = true;
+
+        if (percent < 0)
+        {
+            percent = 0;
+        }
+    }
+
+    public void Reset()
+    {
+        percent = 0;
+    }
+
+    public void AddHouse(float magnitude)
+    {
+        Add(magnitude);
+        other.Sub(magnitude);
+    }
+}
--- Assets/Scripts/Components/Tester.cs
+++ Assets/Scripts/Components/Tester.cs
@@ -12,7 +12,7 @@ public class Tester : MonoBehaviour
     {
         if (test)
         {
-            obj.GetComponent<Meter>().Sub(0.2f);
+            obj.GetComponent<HouseMeter>().AddHouse(0.2f);
             test = false;
         }
     }
