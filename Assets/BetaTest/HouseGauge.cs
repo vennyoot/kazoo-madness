3ae00b9f629bd62898d98bb5facc49cbbec3fff8
Zoe C. | Venn <38826285+vennyoot@users.noreply.github.com>
2020-03-05 18:11:33 -0500

    gauge edit & title menu

--- Assets/BetaTest/HouseGauge.cs
+++ Assets/BetaTest/HouseGauge.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HouseGauge : Gauge
 {
+    UnityEvent onEmpty;
+
     private void Start()
     {
         startFill = 1;
