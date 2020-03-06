81edcabee8b80d8126a39ace7e7634595fc7c236
Zoe C. | Venn <38826285+vennyoot@users.noreply.github.com>
2020-03-05 21:01:33 -0500

    audio changs

--- Assets/BetaTest/AudioStuff/BackgroundMusic.cs
+++ Assets/BetaTest/AudioStuff/BackgroundMusic.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BackgroundMusic : AudioBase
 {
-    AudioClip backgroundMusic;
+    public AudioClip backgroundMusic;
     private void Start()
     {
         audio.loop = true;
